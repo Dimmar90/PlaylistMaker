e34874c71b9b18eaecc035d7f9bf21a10d51fc4c
Dimmar90 <dmitriy.mariyevskiy@gmail.com>
2024-09-07 22:03:37 +0300

    Add Edits

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -5,12 +5,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.internal.ViewUtils.hideKeyboard
+
 
 class SearchActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId", "ResourceAsColor", "WrongViewCast")
@@ -37,14 +42,30 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Toast.makeText(this@SearchActivity, "Поиск: " + s, Toast.LENGTH_SHORT)
-                    .show()
+
             }
 
             override fun afterTextChanged(s: Editable?) {
             }
         }
         (editText as EditText).addTextChangedListener(textWatcher)
+
+        val searchCloseButtonId =
+            searchView.findViewById<View>(androidx.appcompat.R.id.search_close_btn).id
+        val closeButton = searchView.findViewById<ImageView>(searchCloseButtonId)
+        closeButton.setOnClickListener {
+            (editText as EditText).text.clear()
+
+            val view: View? = this.currentFocus
+
+            if (view != null) {
+
+                val inputMethodManager =
+                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+
+                inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0)
+            }
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
