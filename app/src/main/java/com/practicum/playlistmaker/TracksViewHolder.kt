a91cbd3e2b3dda7df3c31cb7822269a6316c721f
Dimmar90 <dmitriy.mariyevskiy@gmail.com>
2024-09-15 18:53:13 +0300

    Add Tracks

---
+++ app/src/main/java/com/practicum/playlistmaker/TracksViewHolder.kt
@@ -0,0 +1,44 @@
+package com.practicum.playlistmaker
+
+import android.content.Context
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TracksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val trackNameView: TextView
+    private val artistNameView: TextView
+    private val trackTimeView: TextView
+    private val artworkView: ImageView
+
+    init {
+        trackNameView = itemView.findViewById(R.id.trackName)
+        artistNameView = itemView.findViewById(R.id.artistName)
+        trackTimeView = itemView.findViewById(R.id.trackTime)
+        artworkView = itemView.findViewById(R.id.cover)
+    }
+
+    fun dpToPx(dp: Float, context: Context): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics).toInt()
+    }
+
+    fun bind(model: Track) {
+        trackNameView.text = model.trackName
+        artistNameView.text = model.artistName
+        trackTimeView.text = model.trackTime
+        Glide.with(itemView)
+            .load(model.artworkUrl100)
+            .centerInside()
+            .transform(RoundedCorners(8))
+            .placeholder(R.drawable.placeholder_icon)
+            .into(artworkView)
+    }
+}
