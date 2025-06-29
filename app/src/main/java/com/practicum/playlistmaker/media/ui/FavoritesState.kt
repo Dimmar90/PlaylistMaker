7d3992371efbcc79c9905f6de823ea11caa9c59d
Dimmar90 <dmitriy.mariyevskiy@gmail.com>
2025-06-29 18:41:44 +0300

    app refactoring

--- app/src/main/java/com/practicum/playlistmaker/media/ui/FavoritesState.kt
+++ app/src/main/java/com/practicum/playlistmaker/media/ui/FavoritesState.kt
@@ -3,6 +3,7 @@ package com.practicum.playlistmaker.media.ui
 import com.practicum.playlistmaker.search.domain.models.Track
 
 sealed class FavoritesState {
+
     data object StateEmpty : FavoritesState()
     data class StateContent(val tracks: List<Track>) : FavoritesState()
 }
